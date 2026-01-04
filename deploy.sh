a646bd46e52d3ad8363fb5764192bcb9299c3712
M0JXD <jamiedrinkell@gmail.com>
2026-01-04 20:35:15 +0000

    update possibly catastrophic deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 # _site is gitignored in both branchs and persists between checkouts
 # So we can simply copy it's contents upwards overwriting anything
 set -e
+shopt -s extglob
 echo "Running git status, you should only run this script if everything is committed..."
 git status
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 git checkout deploy
+rm -rf !\(_site|.git|.gitignore)
 cp -R _site/* .
 git commit -m "Update deployment"
 git checkout main
