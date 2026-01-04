f09d1c6482283674b8938cfde846d7d6b57bdade
M0JXD <jamiedrinkell@gmail.com>
2026-01-04 20:31:46 +0000

    add untested deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# _site is gitignored in both branchs and persists between checkouts
+# So we can simply copy it's contents upwards overwriting anything
+set -e
+echo "Running git status, you should only run this script if everything is committed..."
+git status
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+git checkout deploy
+cp -R _site/* .
+git commit -m "Update deployment"
+git checkout main
+echo "Deployed to branch"
+
