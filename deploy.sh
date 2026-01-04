1f3397910bff20ff1f3a67028ddc77d6e494777c
M0JXD <jamiedrinkell@gmail.com>
2026-01-04 20:49:09 +0000

    Keep trying to fix and improve deployment

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,22 @@
 # _site is gitignored in both branchs and persists between checkouts
 # So we can simply copy it's contents upwards overwriting anything
 set -e
-shopt -s extglob
-echo "Running git status, you should only run this script if everything is committed..."
+echo -e "Generating site...\n"
+npm install
+echo -e "Running git status, you should only run this script if everything is committed...\n"
 git status
+echo -e "\n"
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo -e "\nCheckout Deploy Branch...\n"
 git checkout deploy
+echo -e "Removing old files...\n"
+shopt -s extglob
 rm -rfv !(_site|.git|.gitignore)
+echo -e "\nCopying new files...\n"
 cp -Rv _site/* .
+echo -e "\nCommiting...\n"
 git add -A
 git commit -m "Update deployment"
+git push
 git checkout main
-echo "Deployed to branch"
+echo -e "\nDeployed!"
