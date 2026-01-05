ad5413567fe4ed1fa0db0900172e5ff08c321540
M0JXD <jamiedrinkell@gmail.com>
2026-01-05 14:08:21 +0000

    WIP Dark Mode stuff
    Is changing a stylesheet really the best? I'm not even sure how it
    should be achieved

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,11 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("bundle.css");
+	eleventyConfig.addPassthroughCopy("bundle-dark.css");
 	eleventyConfig.setInputDirectory("src");
 	eleventyConfig.addPassthroughCopy("img");
 };
 
 module.exports.config = {
-
 	markdownTemplateEngine: "njk",
 	htmlTemplateEngine: "njk",
 };
