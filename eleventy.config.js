3683025a2b0fe3d597243605ba06928af7868dc8
M0JXD <jamiedrinkell@gmail.com>
2026-01-09 20:26:45 +0000

    Dark mode support
    Not sure if I'll bother with a button

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("bundle.css");
-	eleventyConfig.addPassthroughCopy("bundle-dark.css");
 	eleventyConfig.setInputDirectory("src");
 	eleventyConfig.addPassthroughCopy("img");
 };
