cf14332ea38f03eef666bdfcacc27e6e8d77863d
M0JXD <jamiedrinkell@gmail.com>
2026-01-15 04:57:27 +0000

    Add created and updated implementation

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,17 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("bundle.css");
 	eleventyConfig.setInputDirectory("src");
 	eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addFilter("ddmmyy", (dateObj) => {
+      return DateTime.fromJSDate(dateObj).toFormat("dd/MM/yy");
+    });
 };
 
 module.exports.config = {
 	markdownTemplateEngine: "njk",
 	htmlTemplateEngine: "njk",
 };
+
+
