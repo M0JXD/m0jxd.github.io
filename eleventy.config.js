8ced9d261af4ac355538719ef970dc5b02ccaede
M0JXD <jamiedrinkell@gmail.com>
2026-01-15 19:25:51 +0000

    Use longer date format
    It looks nicer

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,8 +4,8 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("bundle.css");
 	eleventyConfig.setInputDirectory("src");
 	eleventyConfig.addPassthroughCopy("img");
-    eleventyConfig.addFilter("ddmmyy", (dateObj) => {
-      return DateTime.fromJSDate(dateObj).toFormat("dd/MM/yy");
+    eleventyConfig.addFilter("datefilter", (dateObj) => {
+      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_HUGE);
     });
 };
 
